e4c3b1f9ec2c677761f34559a22cf700fcd0d057
mminh6823 <minhmacao1280@gmail.com>
2025-03-06 19:24:11 +0700

    init

---
+++ Apriori_Check/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Apriori_Check
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.choosefile = new System.Windows.Forms.Button();
+            this.displayfile = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itemset = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMinsup = new System.Windows.Forms.TextBox();
+            this.btnAction = new System.Windows.Forms.Button();
+            this.listResult = new System.Windows.Forms.ListView();
+            this.lbtapphobien = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // choosefile
+            // 
+            this.choosefile.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.choosefile.Location = new System.Drawing.Point(430, 106);
+            this.choosefile.Name = "choosefile";
+            this.choosefile.Size = new System.Drawing.Size(87, 31);
+            this.choosefile.TabIndex = 0;
+            this.choosefile.Text = "Chọn File";
+            this.choosefile.UseVisualStyleBackColor = true;
+            this.choosefile.Click += new System.EventHandler(this.choosefile_Click);
+            // 
+            // displayfile
+            // 
+            this.displayfile.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.displayfile.Location = new System.Drawing.Point(124, 110);
+            this.displayfile.Name = "displayfile";
+            this.displayfile.Size = new System.Drawing.Size(288, 22);
+            this.displayfile.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tid,
+            this.itemset});
+            this.dataGridView1.Location = new System.Drawing.Point(124, 257);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(670, 120);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // tid
+            // 
+            this.tid.DataPropertyName = "tid";
+            this.tid.HeaderText = "TID";
+            this.tid.MinimumWidth = 6;
+            this.tid.Name = "tid";
+            this.tid.Width = 125;
+            // 
+            // itemset
+            // 
+            this.itemset.DataPropertyName = "itemset";
+            this.itemset.HeaderText = "Itemset";
+            this.itemset.MinimumWidth = 6;
+            this.itemset.Name = "itemset";
+            this.itemset.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File .SVM";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Min Support (Ngưỡng hỗ trợ tối thiểu)";
+            // 
+            // txtMinsup
+            // 
+            this.txtMinsup.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtMinsup.Location = new System.Drawing.Point(124, 178);
+            this.txtMinsup.Name = "txtMinsup";
+            this.txtMinsup.Size = new System.Drawing.Size(288, 22);
+            this.txtMinsup.TabIndex = 5;
+            // 
+            // btnAction
+            // 
+            this.btnAction.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAction.Location = new System.Drawing.Point(430, 174);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(87, 31);
+            this.btnAction.TabIndex = 0;
+            this.btnAction.Text = "Thực thi thuật toán";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // listResult
+            // 
+            this.listResult.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listResult.HideSelection = false;
+            this.listResult.Location = new System.Drawing.Point(124, 402);
+            this.listResult.Name = "listResult";
+            this.listResult.Size = new System.Drawing.Size(670, 97);
+            this.listResult.TabIndex = 6;
+            this.listResult.UseCompatibleStateImageBehavior = false;
+            // 
+            // lbtapphobien
+            // 
+            this.lbtapphobien.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbtapphobien.AutoSize = true;
+            this.lbtapphobien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtapphobien.Location = new System.Drawing.Point(536, 112);
+            this.lbtapphobien.Name = "lbtapphobien";
+            this.lbtapphobien.Size = new System.Drawing.Size(258, 25);
+            this.lbtapphobien.TabIndex = 7;
+            this.lbtapphobien.Text = "Số tập phổ biến tìm được là: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 609);
+            this.Controls.Add(this.lbtapphobien);
+            this.Controls.Add(this.listResult);
+            this.Controls.Add(this.txtMinsup);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.displayfile);
+            this.Controls.Add(this.btnAction);
+            this.Controls.Add(this.choosefile);
+            this.Name = "Form1";
+            this.Text = "Tìm tập phổ biến bảng thuật toán Apriori";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button choosefile;
+        private System.Windows.Forms.TextBox displayfile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMinsup;
+        private System.Windows.Forms.Button btnAction;
+        private System.Windows.Forms.ListView listResult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itemset;
+        private System.Windows.Forms.Label lbtapphobien;
+    }
+}
+
---
+++ Apriori_Check/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Apriori_Check
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<string>> transactions = new Dictionary<string, List<string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupDataGridView();
+            SetupListView();
+        }
+        private void SetupDataGridView()
+        {
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.AllowUserToResizeRows = false;
+        }
+        private void SetupListView()
+        {
+            listResult.View = View.Details;
+            listResult.Columns.Clear();
+            listResult.Columns.Add("Itemset", 200, HorizontalAlignment.Left);
+            listResult.Columns.Add("Support", 100, HorizontalAlignment.Right);
+        }
+        private void LoadTransactions(string filePath)
+        {
+            transactions.Clear();
+            dataGridView1.Rows.Clear();
+
+            foreach (var line in File.ReadLines(filePath))
+            {
+                var parts = line.Split(new[] { ' ' }, 2);
+                if (parts.Length == 2)
+                {
+                    string tid = parts[0];
+                    List<string> items = parts[1].Split(',').Select(x => x.Trim()).ToList();
+                    transactions[tid] = items;
+
+                    dataGridView1.Rows.Add(tid, string.Join(", ", items));
+                }
+            }
+        }
+
+        private void choosefile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "SVM Files (*.svm)|*.svm|All Files (*.*)|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    displayfile.Text = openFileDialog.FileName;
+                    LoadTransactions(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void btnAction_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtMinsup.Text, out double minSupport) || minSupport <= 0 || minSupport > 1)
+            {
+                MessageBox.Show("Vui lòng nhập ngưỡng hỗ trợ hợp lệ (số thực từ 0 đến 1).");
+                return;
+            }
+
+            var frequentItemsets = AprioriAlgorithm.Run(transactions.Values.ToList(), minSupport);
+            listResult.Items.Clear();
+
+            lbtapphobien.Text = $"Số tập phổ biến tìm được: {frequentItemsets.Count}";
+            if (frequentItemsets.Count == 0)
+            {
+                MessageBox.Show("Không có tập phổ biến nào đạt ngưỡng hỗ trợ!");
+                return;
+            }
+
+            foreach (var itemset in frequentItemsets)
+            {
+                var row = new ListViewItem(string.Join(", ", itemset.Key));
+                row.SubItems.Add(itemset.Value.ToString("F4"));  
+                listResult.Items.Add(row);
+            }
+        }
+
+
+    }
+
+    public static class AprioriAlgorithm
+    {
+        public static Dictionary<HashSet<string>, double> Run(List<List<string>> transactions, double minSupport)
+        {
+            Dictionary<HashSet<string>, double> frequentItemsets = new Dictionary<HashSet<string>, double>(HashSet<string>.CreateSetComparer());
+            int totalTransactions = transactions.Count;
+
+            var itemCounts = transactions.SelectMany(t => t)
+                                         .GroupBy(i => i)
+                                         .ToDictionary(g => new HashSet<string> { g.Key }, g => (double)g.Count() / totalTransactions);
+
+            var currentItemsets = itemCounts.Where(i => i.Value >= minSupport)
+                                            .ToDictionary(i => i.Key, i => i.Value);
+
+            frequentItemsets = new Dictionary<HashSet<string>, double>(currentItemsets);
+
+            int k = 2;
+            while (currentItemsets.Count > 0)
+            {
+                var candidateItemsets = GenerateCandidates(currentItemsets.Keys.ToList(), k);
+                var candidateCounts = new Dictionary<HashSet<string>, double>(HashSet<string>.CreateSetComparer());
+
+                foreach (var transaction in transactions)
+                {
+                    foreach (var candidate in candidateItemsets)
+                    {
+                        if (candidate.IsSubsetOf(transaction))
+                        {
+                            if (!candidateCounts.ContainsKey(candidate))
+                                candidateCounts[candidate] = 0;
+                            candidateCounts[candidate] += 1.0 / totalTransactions;
+                        }
+                    }
+                }
+
+                currentItemsets = candidateCounts.Where(c => c.Value >= minSupport)
+                                                 .ToDictionary(c => c.Key, c => c.Value);
+
+                foreach (var item in currentItemsets)
+                    frequentItemsets[item.Key] = item.Value;
+
+                k++;
+            }
+            return frequentItemsets;
+        }
+
+        private static List<HashSet<string>> GenerateCandidates(List<HashSet<string>> prevItemsets, int k)
+        {
+            var candidates = new List<HashSet<string>>();
+
+            for (int i = 0; i < prevItemsets.Count; i++)
+            {
+                for (int j = i + 1; j < prevItemsets.Count; j++)
+                {
+                    var unionSet = new HashSet<string>(prevItemsets[i]);
+                    unionSet.UnionWith(prevItemsets[j]);
+
+                    if (unionSet.Count == k && !candidates.Contains(unionSet))
+                        candidates.Add(unionSet);
+                }
+            }
+            return candidates;
+        }
+    }
+}
